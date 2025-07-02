4d189ebfe1cb4effb60dc2a81d6f6a7a2c0004fd
viannik <ivan.nikitski@gmail.com>
2025-07-03 00:33:32 +0300

    add cafe and error handling for visitor requirements

---
+++ app/cafe.py
@@ -0,0 +1,20 @@
+from errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError,
+)
+from datetime import date
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor or not visitor["vaccine"]:
+            raise NotVaccinatedError("Visitor is not vaccinated")
+        if visitor["vaccine"]["expiration_date"] < date.today():
+            raise OutdatedVaccineError("Visitor's vaccine is outdated")
+        if not visitor["wearing_a_mask"]:
+            raise NotWearingMaskError("Visitor is not wearing a mask")
+        return f"Welcome to {self.name}"
